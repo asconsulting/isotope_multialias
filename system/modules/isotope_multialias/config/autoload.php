9092a44a95193fad2c4b3e619bfad6cb22a94836
asconsulting <andrew@andrewstevens.consulting>
2019-02-04 12:18:43 -0500

    Autoload

--- system/modules/isotope_multialias/config/autoload.php
+++ system/modules/isotope_multialias/config/autoload.php
@@ -16,8 +16,8 @@
  */
 ClassLoader::addClasses(array
 (
-    'IsotopeAsc\Backend\Attribute\MultiAliasCallback' 	=> 'system/modules/isotope_multialias/library/Isotope/Backend/Attribute/MultiAliasCallback.php',
-	'IsotopeAsc\Model\Attribute\AlternateAlias' 		=> 'system/modules/isotope_multialias/library/Isotope/Model/Attribute/AlternateAlias.php',
-	'IsotopeAsc\Model\Product\MultiAlias' 				=> 'system/modules/isotope_multialias/library/Isotope/Model/Product/MultiAlias.php',
-	'IsotopeAsc\Module\MultiAliasProductReader' 		=> 'system/modules/isotope_multialias/library/Isotope/Module/MultiAliasProductReader.php'
+    'IsotopeAsc\Backend\Attribute\MultiAliasCallback' 	=> 'system/modules/isotope_multialias/library/IsotopeAsc/Backend/Attribute/MultiAliasCallback.php',
+	'IsotopeAsc\Model\Attribute\AlternateAlias' 		=> 'system/modules/isotope_multialias/library/IsotopeAsc/Model/Attribute/AlternateAlias.php',
+	'IsotopeAsc\Model\Product\MultiAlias' 				=> 'system/modules/isotope_multialias/library/IsotopeAsc/Model/Product/MultiAlias.php',
+	'IsotopeAsc\Module\MultiAliasProductReader' 		=> 'system/modules/isotope_multialias/library/IsotopeAsc/Module/MultiAliasProductReader.php'
 ));
